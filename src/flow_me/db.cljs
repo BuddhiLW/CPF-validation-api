775979a1ef2401b9933bb9694d07a16b195ff93b
BuddhiLW <pedrogbranquinho@gmail.com>
2021-06-25 17:06:17 -0300

    first commit

---
+++ src/flow_me/db.cljs
@@ -0,0 +1,4 @@
+(ns flow-me.db)
+
+(def default-db
+  {:name "re-frame"})
---
+++ src/flow_me/events.cljs
@@ -0,0 +1,11 @@
+(ns flow-me.events
+  (:require
+   [re-frame.core :as re-frame]
+   [flow-me.db :as db]
+   [day8.re-frame.tracing :refer-macros [fn-traced]]
+   ))
+
+(re-frame/reg-event-db
+ ::initialize-db
+ (fn-traced [_ _]
+   db/default-db))
